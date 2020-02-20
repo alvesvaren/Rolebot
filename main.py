9fcd7c65a7f2f661c41be31c3d65265c0afdc140
Alve Svarén <alve@hotmail.se>
2020-02-20 18:56:04 +0100

    First version

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import discord
+import discord.ext.commands
+
+data = {
+    "token": "",
+    "prefix": "!"
+}
+
+try:
+    with open("config.json") as file:
+        data = json.load(file)
+except FileNotFoundError:
+    with open("config.json", "w") as file:
+        json.dump(data, file, indent=4)
+        print("Created default config file. Modify it and run the program again")
+
+bot = discord.ext.commands.Bot(command_prefix="!")
+
+@bot.command()
+async def test(ctx):
+    pass
+
+bot.start("")
