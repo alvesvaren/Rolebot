d910f6eb7adf8f97725a2d86df7eb5c84bfab1bd
Alve Svarén <alve@hotmail.se>
2020-02-20 20:52:54 +0100

    Added more checks with invalid arguments and an ability to check current info about your role

--- main.py
+++ main.py
@@ -20,15 +20,25 @@ except FileNotFoundError:
 bot = discord.ext.commands.Bot(command_prefix=data["prefix"])
 
 @bot.command()
-async def role(ctx: discord.ext.commands.Context, name: str, color: discord.ext.commands.ColourConverter):
+async def role(ctx: discord.ext.commands.Context, name: str = None, color: discord.ext.commands.ColourConverter = None):
     print(f"{ctx.author}: {ctx.message.content}")
+    if not color:
+        color = ctx.author.top_role.color
     async with ctx.typing():
-        if len(ctx.author.roles) > 1:
-            await ctx.author.top_role.edit(color=color, name=name, hoist=True)
+        if name:
+            if len(ctx.author.roles) > 1:
+                await ctx.author.top_role.edit(color=color, name=name, hoist=True)
+            else:
+                await ctx.author.add_roles(await ctx.guild.create_role(color=color, name=name, hoist=True))
+            embed = discord.Embed(title=f"Set {ctx.author.name}'s role to \"{name}\" with the color `{color}`")
+            embed.color = color
+            await ctx.send(embed=embed)
         else:
-            await ctx.author.add_roles(await ctx.guild.create_role(color=color, name=name, hoist=True))
-        embed = discord.Embed(title=f"Set {ctx.author.name}'s role to \"{name}\" with the color `{color}`")
-        embed.color = color
-        await ctx.send(embed=embed)
+            if len(ctx.author.roles) > 1:
+                embed = discord.Embed(title=f"Your role name is \"{ctx.author.top_role.name}\" and has the color `{ctx.author.top_role.color}`")
+                embed.color = ctx.author.top_role.color
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("It looks like you need to set a role first. Try `!role \"role name\" #789abc`")
 
 bot.run(data["token"])
