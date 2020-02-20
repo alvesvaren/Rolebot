cf9df327f6398711b6adb0c274bd95ae12b4ec50
Alve Svarén <alve@hotmail.se>
2020-02-20 20:58:37 +0100

    Updated bot

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ except FileNotFoundError:
     with open("config.json", "w") as file:
         json.dump(data, file, indent=4)
         print("Created default config file. Modify it and run the program again")
+        quit()
 
 bot = discord.ext.commands.Bot(command_prefix=data["prefix"])
 
