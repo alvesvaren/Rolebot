8a9de47e6845d5a5d7594d7cedc71ad841ad3a9b
Alve Svarén <alve@hotmail.se>
2020-02-20 20:33:49 +0100

    The bot now works

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ data = {
     "prefix": "!"
 }
 
+color_converter = discord.ext.commands.ColourConverter()
+
 try:
     with open("config.json") as file:
         data = json.load(file)
@@ -18,7 +20,15 @@ except FileNotFoundError:
 bot = discord.ext.commands.Bot(command_prefix=data["prefix"])
 
 @bot.command()
-async def test(ctx):
-    pass
+async def role(ctx: discord.ext.commands.Context, name: str, color: discord.ext.commands.ColourConverter):
+    print(f"{ctx.author}: {ctx.message.content}")
+    async with ctx.typing():
+        if len(ctx.author.roles) > 1:
+            await ctx.author.top_role.edit(color=color, name=name, hoist=True)
+        else:
+            await ctx.author.add_roles(await ctx.guild.create_role(color=color, name=name, hoist=True))
+        embed = discord.Embed(title=f"Set {ctx.author.name}'s role to \"{name}\" with the color `{color}`")
+        embed.color = color
+        await ctx.send(embed=embed)
 
 bot.run(data["token"])
